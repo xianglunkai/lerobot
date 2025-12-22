32b7e79530a2daaff5aa846554e7cf9e80bc99f4
xianglunkai <1322099375@qq.com>
2025-12-22 17:45:36 +0800

    fix lerobot3.0->lerobot2.1

--- scripts/data_transformer.sh
+++ scripts/data_transformer.sh
@@ -10,24 +10,22 @@ export TOKENIZERS_PARALLELISM=false
 # 环境变量: 指定 HuggingFace 数据集本地缓存（离线模式需要）
 export HF_DATASETS_CACHE=/workspace/huggingface/lerobot
 
+python src/lerobot/datasets/v30/convert_dataset_v30_to_v21.py \
+    --repo-id lerobot/handover_bottle \
+    --root ../huggingface/ \
+    --offline \
+    --embed-images
+
 python src/lerobot/datasets/v30/hdf5_to_lerobot_dobot_v3.py \
   --input_dir ../datasets/fold_towel/ \
   --repo_id lerobot/fold_towel_v3_0 \
   --root ../huggingface/lerobot/fold_towel_v3_0
 
-# python src/lerobot/datasets/v30/convert_dataset_v30_to_v21.py \
-#   --repo-id handover_bottle_action_from_slave_3_0 \
-#   --root /workspace/huggingface/lerobot \
-#   --offline \
-#   --embed-images
+
+python -m lerobot.datasets.v30.convert_dataset_v21_to_v30 \
+  --repo-id fold_towel \
+  --root /workspace/huggingface/lerobot \
+  --push-to-hub=false
 
 
-# python -m lerobot.datasets.v30.convert_dataset_v21_to_v30 \
-#   --repo-id fold_towel \
-#   --root /workspace/huggingface/lerobot \
-#   --push-to-hub=false
 
-# python src/lerobot/datasets/v30/convert_dataset_v20_to_v21.py \
-#   --repo-id handover_bottle_action_from_slave_2_0 \
-#   --root /workspace/huggingface/lerobot \
-#   --offline --no-push 
