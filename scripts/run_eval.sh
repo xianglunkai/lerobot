80e841c256cb526a5ed4c9fbac1aed8c12326cda
xianglunkai <1322099375@qq.com>
2026-01-04 15:31:38 +0800

    sarm alogrithm debug and test

--- scripts/run_eval.sh
+++ scripts/run_eval.sh
@@ -5,3 +5,11 @@ python -m lerobot.scripts.eval \
     --eval.n_episodes=10 \
     --use_amp=false \
     --device=cuda
+
+lerobot-eval \
+    --policy.path=/home/xlk/work/lerobot/checkpoints/fold_towel/30k-50hz/pretrained_model \
+    --env.type=cobot \
+    --eval.batch_size=10 \
+    --eval.n_episodes=10 \
+    --policy.use_amp=false \
+    --policy.device=cuda
--- scripts/run_rtc.sh
+++ scripts/run_rtc.sh
@@ -5,8 +5,8 @@ python examples/rtc/eval_with_real_robot.py \
     --policy.path=/home/xlk/work/lerobot/checkpoints/fold_towel/30k-50hz/pretrained_model \
     --policy.device=cuda \
     --robot.type=agilex_cobot \
-    --rtc.enabled=true \
-    --rtc.execution_horizon=25 \
+    --rtc.enabled=false \
+    --rtc.execution_horizon= 25\
     --task="Carefully fold the towel and then place the folded towel on the black notebook" \
     --duration=240 \
     --fps=50 \
