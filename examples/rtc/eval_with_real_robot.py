0d87229c528803e81588ed04ff91609ea7979537
xianglunkai <1322099375@qq.com>
2026-01-04 15:31:38 +0800

    sarm alogrithm debug and test

--- examples/rtc/eval_with_real_robot.py
+++ examples/rtc/eval_with_real_robot.py
@@ -145,7 +145,7 @@ class RTCDemoConfig(HubMixin):
     rtc: RTCConfig = field(
         default_factory=lambda: RTCConfig(
             execution_horizon=10,
-            max_guidance_weight=1.0,
+            max_guidance_weight=10.0,
             prefix_attention_schedule=RTCAttentionSchedule.EXP,
         )
     )
