d0ccad63b856eaa6dae8873cf45c7d9b8708477c
xianglunkai <1322099375@qq.com>
2026-01-27 14:01:40 +0800

    improve rtc alogrithm

--- scripts/run_eval_rtc.sh
+++ scripts/run_eval_rtc.sh
@@ -3,12 +3,13 @@ export HF_HOME=/data/huggingface
     
 python examples/rtc/eval_dataset.py \
         --policy.path=/home/xlk/work/lerobot/checkpoints/fold_towel/030000/pretrained_model \
-        --dataset.repo_id=lerobot/eval_lerobot_fold_towel_20260114_141605 \
+        --dataset.repo_id=lerobot/eval_lerobot_fold_towel_20260116_110149 \
         --rtc.enabled=True \
-        --rtc.execution_horizon=18 \
-        --rtc.max_guidance_weight=15.0 \
+        --rtc.execution_horizon=15 \
+        --rtc.max_guidance_weight=10.0 \
         --rtc.debug=True \
         --rtc.debug_maxlen=1000 \
-        --rtc.prefix_attention_schedule=EXP \
-        --inference_delay=8 \
+        --inference_delay=10 \
         --device=cuda \
+        --rtc.sigma_d=1.0 \
+        --rtc.full_trajectory_alignment=False \
