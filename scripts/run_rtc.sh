9d42cd50d6bae27c5e6871fd37ff6d1b62c93b6c
xianglunkai <1322099375@qq.com>
2026-02-12 16:56:30 +0800

    fix rtc

--- scripts/run_rtc.sh
+++ scripts/run_rtc.sh
@@ -2,7 +2,7 @@ export HF_LEROBOT_HOME=/data/huggingface/lerobot
 export HF_HOME=/data/huggingface
     
 python examples/rtc/eval_with_real_robot.py \
-    --policy.path=/home/xlk/work/lerobot/checkpoints/lerobot_fold_cloth_v3.0/checkpoints/030000/pretrained_model \
+    --policy.path=/home/xlk/work/lerobot/checkpoints/fold_towel/030000/pretrained_model \
     --policy.device=cuda \
     --robot.type=agilex_cobot \
     --rtc.enabled=True \
@@ -14,7 +14,7 @@ python examples/rtc/eval_with_real_robot.py \
     --duration=60 \
     --fps=30 \
     --device=cuda \
-    --action_queue_size_to_get_new_actions=25 \
+    --action_queue_size_to_get_new_actions=30 \
     --use_torch_compile=False \
     --enable_visualization=False \
     --interpolation_multiplier=10 \
