2230e83e865fe69cd89c8a397c63ff01bd55e7d5
xianglunkai <1322099375@qq.com>
2026-02-23 17:42:08 +0800

    add mpc processor plugin

--- src/lerobot/processor/__init__.py
+++ src/lerobot/processor/__init__.py
@@ -92,6 +92,7 @@ from .relative_action_processor import (
 from .rename_processor import RenameObservationsProcessorStep
 from .tokenizer_processor import ActionTokenizerProcessorStep, TokenizerProcessorStep
 from .filter_processor import LowPassFilterProcessor
+from .qp_action_processor import QPActionSmoothingProcessor
 
 __all__ = [
     "ActionProcessorStep",
@@ -151,6 +152,7 @@ __all__ = [
     "UnnormalizerProcessorStep",
     "VanillaObservationProcessorStep",
     "LowPassFilterProcessor",
+    "QPActionSmoothingProcessor",
     "to_absolute_actions",
     "to_delta_actions",
 ]
